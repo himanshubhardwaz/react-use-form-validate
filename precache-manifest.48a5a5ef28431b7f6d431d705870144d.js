71e9a7abe993a50b6a8737e1b21f8a062b50cab9
Himanshu Bhardwaz <himanshu76200@gmail.com>
2021-12-26 10:02:33 +0530

    Updates

---
+++ precache-manifest.48a5a5ef28431b7f6d431d705870144d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e619d4abafa854dd137c2d2e4c57b8",
+    "url": "/react-use-form-validate/index.html"
+  },
+  {
+    "revision": "b148e61bb7ff900efc57",
+    "url": "/react-use-form-validate/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "be51cc13a6dc7e55605f",
+    "url": "/react-use-form-validate/static/js/2.76afc5b2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-use-form-validate/static/js/2.76afc5b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b148e61bb7ff900efc57",
+    "url": "/react-use-form-validate/static/js/main.1116c769.chunk.js"
+  },
+  {
+    "revision": "7d55cd1d1d6c5079647d",
+    "url": "/react-use-form-validate/static/js/runtime-main.30b38ebb.js"
+  }
+]);
